50a2325b6f24b63ca6ce207a28a7b4961c9afe81
FelixYu2021202 <1276273366@qq.com>
2024-05-25 22:48:07 +0800

    cfpc v1.1.0

--- cfpc/cfpc.js
+++ cfpc/cfpc.js
@@ -1,13 +1,12 @@
 // ==UserScript==
-// @name         Codeforces Problem Chooser
+// @name         Codeforces & Atcoder Problem Chooser
 // @namespace    http://luogu.com.cn/
-// @version      1.0.1
-// @description  Choose problem from codeforces
+// @version      1.1.0
+// @description  Choose problem from Codeforces and Atcoder
 // @author       cosf
 // @match        https://www.luogu.com.cn/contest/edit/*
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
 // @grant        GM_xmlhttpRequest
-// @connect      https://codeforces.com/*
 // ==/UserScript==
 
 (function () {
@@ -34,7 +33,9 @@
     }
 </style>`).appendTo(document.body);
 
-    let row = $(`<div data-v-581a38cc class="row">
+    //#region cf
+
+    $(`<div data-v-581a38cc class="row">
     <span data-v-581a38cc>
         <span data-v-226afee4 data-v-581a38cc>
             爬取 CF
@@ -42,13 +43,13 @@
     </span>
     <div data-v-581a38cc>
         <p data-v-226afee4 data-v-581a38cc>
-            难度：<span id="difficulty-selector"></span>
+            难度：<span id="cf-difficulty-selector"></span>
         </p>
         <p data-v-226afee4 data-v-581a38cc>
-            已选：<span id="selected-tag"></span>
+            已选：<span id="cf-selected-tag"></span>
         </p>
         <p data-v-226afee4 data-v-581a38cc>
-            未选：<span id="unselected-tag"></span>
+            未选：<span id="cf-unselected-tag"></span>
         </p>
         <p>
             <button data-v-7ade990c data-v-226afee4 type="button" class="lfe-form-sz-small" data-v-581a38cc style="border-color: rgb(52, 152, 219); background-color: rgb(52, 152, 219);" id="cf-search">
@@ -58,13 +59,13 @@
     </div>
 </div>`).insertAfter($(".form-layout").children().first());
 
-    let difselect = $(`<select></select>`).appendTo("#difficulty-selector");
+    let cfdifselect = $(`<select></select>`).appendTo("#cf-difficulty-selector");
 
     for (let i = 800; i < 3600; i += 100) {
-        difselect.append(`<option value="${i}">${i}</option>`);
+        cfdifselect.append(`<option value="${i}">${i}</option>`);
     }
 
-    let tags = [
+    let cftags = [
         "2-sat",
         "binary search",
         "bitmasks",
@@ -103,40 +104,43 @@
         "two pointers"
     ];
 
-    let selectspan = $(`<span class="tag-select"></span>`).appendTo("#selected-tag");
-    let unselectspan = $(`<span class="tag-select"></span>`).appendTo("#unselected-tag");
+    let cfselectspan = $(`<span class="tag-select"></span>`).appendTo("#cf-selected-tag");
+    let cfunselectspan = $(`<span class="tag-select"></span>`).appendTo("#cf-unselected-tag");
 
-    let selected = {}, unselected = {};
+    let cfselected = {}, cfunselected = {};
 
-    let tagsspan = {};
+    let cftagsspan = {};
 
-    tags.forEach(tag => {
-        tagsspan[tag] = $(`<span class="tag-select-span">${tag}</span>`).appendTo(unselectspan);
-        unselected[tag] = true;
-        selected[tag] = false;
-        tagsspan[tag].on("click", () => {
-            if (selected[tag]) {
-                selected[tag] = false;
-                unselected[tag] = true;
-                tagsspan[tag] = tagsspan[tag].detach().appendTo(unselectspan);
+    cftags.forEach(tag => {
+        cftagsspan[tag] = $(`<span class="tag-select-span">${tag}</span>`).appendTo(cfunselectspan);
+        cfunselected[tag] = true;
+        cfselected[tag] = false;
+        cftagsspan[tag].on("click", () => {
+            if (cfselected[tag]) {
+                cfselected[tag] = false;
+                cfunselected[tag] = true;
+                cftagsspan[tag] = cftagsspan[tag].detach().appendTo(cfunselectspan);
             }
             else {
-                selected[tag] = true;
-                unselected[tag] = false;
-                tagsspan[tag] = tagsspan[tag].detach().appendTo(selectspan);
+                cfselected[tag] = true;
+                cfunselected[tag] = false;
+                cftagsspan[tag] = cftagsspan[tag].detach().appendTo(cfselectspan);
             }
         });
     });
 
     $("#cf-search").on("click", () => {
         let dif = difselect.val();
-        let st = tags.filter(tag => selected[tag]).map(st => encodeURIComponent(st)).join(";");
+        let st = tags.filter(tag => cfselected[tag]).map(st => encodeURIComponent(st)).join(";");
         console.log(dif, st);
         GM_xmlhttpRequest({
             url: `https://codeforces.com/api/problemset.problems?tags=${st}`,
             onload(resp) {
                 resp = JSON.parse(resp.response);
                 let probs = resp.result.problems.filter(prob => prob.rating == dif);
+                if (dif < 2600) {
+                    probs = probs.filter(prob => prob.contestId > 1000);
+                }
                 if (probs.length == 0) {
                     alert("No this type of problem!");
                     return;
@@ -144,10 +148,87 @@
                 let sp = probs[Math.floor(Math.random() * probs.length)];
                 let id = `CF${sp.contestId}${sp.index}`;
                 navigator.clipboard.writeText(id);
-                alert("copied");
+                alert(`copied: ${id}`);
             },
             method: "GET",
         })
     });
+
+    //#endregion cf
+
+    //#region at
+
+    $(`<div data-v-581a38cc class="row">
+    <span data-v-581a38cc>
+        <span data-v-226afee4 data-v-581a38cc>
+            爬取 AT
+        </span>
+    </span>
+    <div data-v-581a38cc>
+        <p data-v-226afee4 data-v-581a38cc>
+            难度：<span id="at-difficulty-selector"></span>
+        </p>
+        <p>
+            <button data-v-7ade990c data-v-226afee4 type="button" class="lfe-form-sz-small" data-v-581a38cc style="border-color: rgb(52, 152, 219); background-color: rgb(52, 152, 219);" id="at-search">
+                爬取
+            </button>
+        </p>
+    </div>
+</div>`).insertAfter($(".form-layout").children().first());
+
+    let atdifselect = $(`<select></select>`).appendTo("#at-difficulty-selector");
+
+    let atdifficultiesname = [
+        "brown (400 - 800)",
+        "green (800 - 1200)",
+        "cyan (1200 - 1600)",
+        "blue (1600 - 2000)",
+        "yellow (2000 - 2400)",
+        "orange (2400 - 2800)",
+        "red (2800 - 3200)",
+        "bronze (3200 - 3600)",
+        "silver (3600 - 4000)",
+        "gold (4000 - )"
+    ];
+    let atdifficultiesspan = [
+        [400, 800],
+        [800, 1200],
+        [1200, 1600],
+        [1600, 2000],
+        [2000, 2400],
+        [2400, 2800],
+        [2800, 3200],
+        [3200, 3600],
+        [3600, 4000],
+        [4000, 10000]
+    ]
+
+    for (let i = 0; i < 10; i++) {
+        atdifselect.append(`<option value="${i}">${atdifficultiesname[i]}</option>`);
+    }
+
+    $("#at-search").on("click", () => {
+        let dif = atdifselect.val();
+        console.log(dif);
+        GM_xmlhttpRequest({
+            url: "https://kenkoooo.com/atcoder/resources/problem-models.json",
+            onload(resp) {
+                resp = JSON.parse(resp.response);
+                let probs = [];
+                for (let prob in resp) {
+                    let pd = resp[prob];
+                    if (pd.difficulty >= atdifficultiesspan[dif][0] && pd.difficulty < atdifficultiesspan[dif][1]) {
+                        probs.push(`AT_${prob}`);
+                    }
+                }
+                let id = probs[Math.floor(Math.random() * probs.length)];
+                navigator.clipboard.writeText(id);
+                alert(`copied: ${id}`);
+            },
+            method: "GET",
+        })
+    });
+
+    //#endregion at
 
 })();
