9c25eaa6446baedafbeafd035e8582e28bd7d62b
FelixYu2021202 <1276273366@qq.com>
2024-09-14 21:28:25 +0800

    atlt v1.0.1

--- atlt/atlt.js
+++ atlt/atlt.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Atcoder-Luogu Teleporter
 // @namespace    http://atcoder.jp/
-// @version      v1.0.0
+// @version      v1.0.1
 // @description  Teleport to Luogu problem from Atcoder.
 // @author       cosf
 // @match        https://atcoder.jp/contests/*/tasks/*
@@ -16,5 +16,21 @@ setTimeout(function () {
     if (parsed.length == 5) {
         let p = parsed[4];
         $(`<a class="btn btn-default btn-sm" href="https://www.luogu.com.cn/problem/AT_${p}" target="_blank">Luogu</a>`).appendTo($(`.h2`));
+        $(`<a class="btn btn-default btn-sm">Hide</a>`).on("click", () => {
+            $("#menu-wrap").hide();
+            $("nav").hide();
+            $("#contest-nav-tabs").hide();
+            $(".topcoder-like-circle").hide();
+            $(".h2").hide();
+            $("#task-lang-btn").hide();
+            $("form").hide();
+            $(".a2a_kit").hide();
+            $(".container").last().hide();
+            $("#fixed-server-timer").hide();
+            $("#scroll-page-top").hide();
+            $("hr").first().hide();
+            $("hr").last().hide();
+            $("hr").eq(-2).hide();
+        }).appendTo($(`.h2`));
     }
 }, 1000);
